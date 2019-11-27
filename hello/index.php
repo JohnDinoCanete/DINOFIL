3130afb6fd0dc744e30aff9ef3d2f689789913c9
JohnDinoCanete <45748917+JohnDinoCanete@users.noreply.github.com>
2019-11-28 01:12:16 +0800

    Add files via upload

--- hello/index.php
+++ hello/index.php
@@ -1,3 +1,5 @@
+<?php include('server.php') ?>
+  <?php include('errors.php') ?>
 <html>
 <head>
     <title> AICS PROJECT COMPUTER PROGRAMMING </title>
@@ -7,13 +9,13 @@
     <div class="login-box">
     <img src="avatar.png" class="avatar">
         <h1>Login Here</h1>
-            <form action="index.php" method="POST">
+            <form action="main.php" method="POST">
             <p>Username</p>
-            <input type="text" name="username" placeholder="Enter Username">
+            <input type="text" name="username" placeholder="Enter Username" required>
             <p>Password</p>
-            <input type="password" name="password" placeholder="Enter Password">
-            <input type="submit" name="submit" value="login">
-            <a href="#">Forget Password</a>    
+            <input type="password" name="password" placeholder="Enter Password" required>
+            <input type="submit" name="login_user" value="Login">
+            <a href="register.php">Sign up</a>    
             </form>
         
         
@@ -23,24 +25,24 @@
 </html>
 <?php 
 session_start();
-if(isset($_SESSION['user'])){
+if(isset($_SESSION['username'])){
     header("location:main.php");
 }
 include_once ('config.php');
 if(isset($_POST['login'])){
    $susername = $_POST['username'];
    $spassword = $_POST['password'];
-   $sql = "select * from mydatabase.tusers where username = :user && password = :pass";
+   $sql = "select * from mydatabase.tusers where username = :username && password = :pass";
    $query = $conn -> prepare($sql);
-   $query -> bindParam(':user', $susername);
-   $query -> bindParam(':pass', $spassword);
+   $query -> bindParam(':username', $susername);
+   $query -> bindParam(':password', $spassword);
    $query -> execute();
    while($row = $query->fetch(PDO::FETCH_ASSOC)){
        $nickname = $row['nickname'];
    }
     $result = $query->rowCount();
     if($result > 0){
-        $_SESSION['user'] = "ok";
+        $_SESSION['username'] = $susername;
         $_SESSION['nickname'] = $nickname;
         header("location:main.php");
     }
